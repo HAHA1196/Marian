c82020be841ca59fa5531c94f275118d305fa59e
Jen Hung <jenhung910271@gmail.com>
2021-09-13 16:56:56 +0800

    0913 16:56 new page ejs by jen

--- back-end/routes/backend.js
+++ back-end/routes/backend.js
@@ -1,9 +1,16 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('backend/index.ejs', {});
+router.get("/", function (req, res, next) {
+    res.render("backend/index.ejs", {});
+});
+/* GET main page. 0913 11:21 jen */
+router.get("/main", function (req, res, next) {
+    res.render("backend/bkendMain.ejs", {});
+});
+router.get("/product", function (req, res, next) {
+    res.render("backend/bkendProduct.ejs", {});
 });
 
 module.exports = router;
