1158391b8c85e191609b2fe0d53f399e857b76c0
cora <chancora0808@gmail.com>
2021-09-18 23:47:22 +0800

    img mousemove

--- 前端同學素材放這邊/index/大專/js/home.js
+++ 前端同學素材放這邊/index/大專/js/home.js
@@ -82,34 +82,58 @@ $( ".navbarMenu" ).find( ".hover" ).css( "transition", "transform .5s ease-in-ou
 //     this.style.backgroundPositionX = valueX + "px"
 //     this.style.backgroundPositionY = valueY + "px"
 //   }
-//   window.onload = function() {
-//     var im = document.getElementById("conceptImg");
-//     if (im) {
-//       im.addEventListener("mousemove",conceptBg,false);
-//     }  
-//   }
-  // $(document).ready(function() {
-  //   var movementStrength = 25;
-  //   var height = movementStrength / $(window).height();
-  //   var width = movementStrength / $(window).width();
-  //   $(".conceptCont").mousemove(function(e){
-  //             var pageX = e.pageX - ($(window).width() / 2);
-  //             var pageY = e.pageY - ($(window).height() / 2);
-  //             var newvalueX = width * pageX * -1 - 25;
-  //             var newvalueY = height * pageY * -1 - 50;
-  //             $('.conceptImg').css("background-position", newvalueX+"px "+newvalueY+"px");
-  //   });
-  //   });
-      document.addEventListener("mousemove", parallax);
-      function parallax(event) {
-        this.querySelectorAll(".conceptCont>img").forEach((shift) => {
-          const position = shift.getAttribute("value");
-          const x = (window.innerWidth - event.pageX * position) / 90;
-          const y = (window.innerHeight - event.pageY * position) / 90;
+  // window.onload = function() {
+  //   var im = document.getElementById("conceptImg");
+  //   if (im) {
+  //     im.addEventListener("mousemove",conceptBg,false);
+  //   }  
+  // }
+  $(document).ready(function() {
+    var movementStrength = 25;
+    var height = movementStrength / $(window).height();
+    var width = movementStrength / $(window).width();
+    $(".conceptF").mousemove(function(e){
+              var pageX = e.pageX - ($(window).width() / 2);
+              var pageY = e.pageY - ($(window).height() / 2);
+              var newvalueX = width * pageX * -1 - 25;
+              var newvalueY = height * pageY * -1 - 50;
+              $('.conceptImg').css("background-position", newvalueX+"px "+newvalueY+"px");
+    });
+    });
+      // document.addEventListener("mousemove", parallax);
+      // function parallax(event) {
+      //   this.querySelectorAll(".conceptImg").forEach((shift) => {
+      //     const position = shift.getAttribute("value");
+      //     const x = (window.innerWidth - event.pageX * position) / 90;
+      //     const y = (window.innerHeight - event.pageY * position) / 90;
   
-          shift.style.transform = `translateX(${x}px) translateY(${y}px)`;
-        });
-      }
+      //     shift.style.transform = `translateX(${x}px) translateY(${y}px)`;
+      //   });
+      // }
+
+      window.onmousemove=function(e){
+
+        let moveNum=(e.pageX/innerWidth)*80;
+        let moveNum2=(e.pageY/innerHeight)*80;
+
+        let newNum=((e.pageX - innerWidth/2)/(innerWidth/2));
+        let newNum2=((e.pageY - innerHeight/2)/(innerHeight/2));
+
+        // console.log(newNum)
+        document.querySelector(".conceptImg").style.transform=`translate(${-50 - (newNum*10)}%,${-50 - (newNum2*10)}%)`
+        }
+        
+      window.onmousemove=function(e){
+
+        let moveNum=(e.pageX/innerWidth)*80;
+        let moveNum2=(e.pageY/innerHeight)*80;
+
+        let newNum=((e.pageX - innerWidth/2)/(innerWidth/2));
+        let newNum2=((e.pageY - innerHeight/2)/(innerHeight/2));
+
+        // console.log(newNum)
+        document.querySelector(".conceptBg").style.transform=`translate(${-50 - (newNum*10)}%,${-50 - (newNum2*10)}%)`
+        }
 // Swiper Configuration
 // material
 $(function(){
