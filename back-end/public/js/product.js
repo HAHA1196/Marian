abf5d271faa936cdf48b1ca8e350e087452286fc
Jin Huang <b01100079@outlook.com>
2021-09-27 18:46:08 +0800

    0927 18PM 修改產品頁

--- back-end/public/js/product.js
+++ back-end/public/js/product.js
@@ -1,8 +1,12 @@
 $("#imgEditBtn").on("click", function () {
     alert("edit this yo");
 });
+// --------------------creat new data from database-----------------
+
+
 
 // ---------------------get data from database-----------------------
+
 $(function () {
 //     // 我要撈兩筆不同的資料，
 //     // 因為SQL語法以我的能力實在沒有辦法把他們寫在同一個
@@ -23,22 +27,26 @@ $(function () {
         "產品描述 | "
     ];
     var trList = [
-        "產品圖片 |",
-        "產品種類 |",
-        "產品型號 |",
-        "產品名稱 |",
-        "產品價格 |",
-        "產品尺寸 |",
-        "產品描述 |"
+        "產品圖片 ",
+        "產品種類 ",
+        "產品型號 ",
+        "產品名稱 ",
+        "產品價格 ",
+        "產品尺寸 ",
+        "產品描述 "
     ];
 
     $.get("http://localhost:8001/api/products", function (data) {
         productList = JSON.parse(data);
         // 查看所以產品資料
-        // console.log(orderList);
+        // console.log(productList);
         catchProductList();
     });
 
+    $('#uploadBtn').click(function()
+    {
+        toggleRow(this.parentNode);
+    });
 
     $('#searchBtn').click(function()
     {
@@ -83,8 +91,13 @@ $(function () {
         for (i = 5; i <= 9; i++) {
             // console.log(trChild);
             $(`tr:nth-child(${i})`).append(
-                '<td class="showContent hideRow">123</td>'
+                '<td class="showContent hideRow"><div class="dividingLine"></div><div class="info infoTxt"><ul></ul></div><div class="dividingLine"></div><button id="confirmBtn"><i class="fa fa-check-circle-o" aria-hidden="true"></i>確認修改</button></td>'
             );
+            toggleList.forEach((element) => {
+                $(`tr:nth-child(${i}) ul:nth-child(1)`).append(
+                    $("<li></li>").append($("<span></span>").text(element))
+                );
+            });
         };
     }
 
