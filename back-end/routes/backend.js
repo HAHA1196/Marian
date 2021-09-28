6d1cd676000eeb19db6bdcf5dc8fd43b3e86567c
Jen Hung <jenhung910271@gmail.com>
2021-09-28 17:14:29 +0800

    0928 17:14 member update jen

--- back-end/routes/backend.js
+++ back-end/routes/backend.js
@@ -22,7 +22,7 @@ router.get("/product", function (req, res, next) {
 // http://localhost:8000/backend/member
 router.get("/member", function (req, res, next) {
     req.mysql.query(
-        "SELECT customerId, customerName, customerGender, customerPhone, customerEmail, customerAddress FROM customers; SELECT o.customerId, o.orderId, CONCAT(p.productName,', ', od.quantity,'件, ', (od.quantity * od.productPrice), '元, ',o.orderDate)AS details FROM orderdetails od JOIN orders o USING(orderId) JOIN products p USING(productId) ORDER BY o.customerId",
+        "SELECT customerId, customerName, customerGender, customerPhone, customerEmail, customerAddress FROM customers; SELECT o.customerId, o.orderId, DATE_FORMAT(o.orderDate,'%Y/%m/%d %H:%i') AS orderDate, SUM(od.productPrice * od.quantity)AS total FROM orderdetails od JOIN orders o USING(orderId) GROUP BY orderId",
         [],
         function (err, result) {
             console.log(result);
@@ -32,13 +32,24 @@ router.get("/member", function (req, res, next) {
             });
         }
     );
+    // req.mysql.query(
+    //     "SELECT customerId, customerName, customerGender, customerPhone, customerEmail, customerAddress FROM customers; SELECT o.customerId, o.orderId, CONCAT(p.productName,', ', od.quantity,'件, ', (od.quantity * od.productPrice), '元, ',o.orderDate)AS details FROM orderdetails od JOIN orders o USING(orderId) JOIN products p USING(productId) ORDER BY o.customerId",
+    //     [],
+    //     function (err, result) {
+    //         console.log(result);
+    //         res.render("backend/member.ejs", {
+    //             member: result[0],
+    //             memberOrderHistory: result[1],
+    //         });
+    //     }
+    // );
 });
 
 /* order page */
 // http://localhost:8000/backend/order
 router.get("/order", function (req, res, next) {
     req.mysql.query(
-        "SELECT o.orderId, o.orderDate, CONCAT(o.customerId, ' - ', c.customerName) as customer, SUM(od.productPrice * od.quantity) AS totalPrice FROM orders o JOIN orderdetails od ON (o.orderId = od.orderId) JOIN customers c ON (o.customerId = c.customerId) GROUP BY o.orderId; SELECT od.orderId, CONCAT(p.productName, ', ', p.productSize, ', ', od.quantity, ', ', (p.productPrice * od.quantity)) AS detail FROM orderdetails od JOIN products p ON (od.productId = p.productId) ORDER BY od.orderId;",
+        "SELECT o.orderId, DATE_FORMAT(o.orderDate,'%Y/%m/%d %k:%i') AS orderDate, CONCAT(o.customerId, ' - ', c.customerName) as customer, SUM(od.productPrice * od.quantity) AS totalPrice FROM orders o JOIN orderdetails od ON (o.orderId = od.orderId) JOIN customers c ON (o.customerId = c.customerId) GROUP BY o.orderId; SELECT od.orderId, CONCAT(p.productName, ', ', p.productSize, ', ', od.quantity, ', ', (p.productPrice * od.quantity)) AS detail FROM orderdetails od JOIN products p ON (od.productId = p.productId) ORDER BY od.orderId;",
         [],
         function (err, result) {
             console.log(result);
